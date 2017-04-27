517f04494e88361224d2b0670215c05a0864efcd
jgietz <jgietz@console.com.au>
2017-04-27 13:12:47 +1000

    Added search bar

--- src/components/app.js
+++ src/components/app.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 
 // App
   // Searchbar
+  import SearchBar from '../containers/search_bar';
     //  Search input
     //  Search Button
   // ForecastList
     // Chart
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
