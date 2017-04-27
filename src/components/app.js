b3a932a5aa997f7351de03ee37db23bdc2f7dba8
jgietz <jgietz@console.com.au>
2017-04-27 12:55:38 +1000

    Rough outline

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 
+// App
+  // Searchbar
+    //  Search input
+    //  Search Button
+  // ForecastList
+    // Chart
 export default class App extends Component {
   render() {
     return (
