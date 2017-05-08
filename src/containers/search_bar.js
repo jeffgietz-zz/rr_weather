ee2c465a8fc3869fb03195af8655963131f53b59
jgietz <jgietz@console.com.au>
2017-05-08 15:01:57 +1000

    Reduxed it

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index';
+
 //  Search input
 //  Search Button
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = {term: ''};
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -20,12 +22,13 @@ export default class SearchBar extends Component {
   onFormSubmit(event) {
     event.preventDefault();
     // Fetch weather data
-
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   render() {
     return (
-      <form className="input-group" onFormSubmit={this.onFormSubmit}>
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a 5 day forecast in your favourite cities"
           className="form-control"
@@ -39,3 +42,10 @@ export default class SearchBar extends Component {
     )
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// Null due to saying 'thanks but we don't need a state here'
+export default connect(null, mapDispatchToProps)(SearchBar);
