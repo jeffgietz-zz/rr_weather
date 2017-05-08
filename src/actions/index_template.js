f46c79f0da1c5dd9c237316eed4803369484b0ce
jgietz <jgietz@console.com.au>
2017-05-08 14:10:12 +1000

    ADded axios and request to API

--- src/actions/index_template.js
+++ src/actions/index_template.js
@@ -1,2 +1,17 @@
 // Add API key and rename me to index.js
+import axios from 'axios';
+
 const API_KEY = '';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},au`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
