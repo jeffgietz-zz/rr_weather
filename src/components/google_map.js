16eddc122c1bb6ad1446ce731c0df7c4fea47cf1
Jeffrey Gietz <jeffgietz@gmail.com>
2017-05-10 14:51:44 +1000

    Added map component

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class GoogleMap extends Component {
   componentDidMount(){
-    new google.maps.map(this.refs.map, {
+    new google.maps.Map(this.refs.map, {
       zoom:12,
       center: {
         lat: this.props.lat,
